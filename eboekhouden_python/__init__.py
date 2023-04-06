22f8378441a85ac429d617d0d7fa8c0a2f7643b0
Dennis Bakhuis <git@bakhuis.nu>
2023-04-06 15:25:28 +0200

    removed redundant lines

--- eboekhouden_python/__init__.py
+++ eboekhouden_python/__init__.py
@@ -1,37 +1,8 @@
 """Main init for eboekhouden_python."""
 from .eboekhouden_client import EboekhoudenClient
 
-# from .constants import (
-#     BedrijfParticulier,
-#     BtwCode,
-#     btw_codes_hoog,
-#     btw_codes_laag,
-#     btw_codes_geen,
-#     InExBTW,
-#     MutatieSoort,
-# )
-# from .models import (
-#     Mutatie,
-#     MutatieFilter,
-#     MutatieRegel,
-#     Relatie,
-#     RelatieFilter,
-# )
-
 
 __version__ = "0.1.7"
 __all__ = [
-    # "BedrijfParticulier",
-    # "BtwCode",
-    # "btw_codes_hoog",
-    # "btw_codes_laag",
-    # "btw_codes_geen",
     "EboekhoudenClient",
-    # "InExBTW",
-    # "MutatieSoort",
-    # "Mutatie",
-    # "MutatieFilter",
-    # "MutatieRegel",
-    # "Relatie",
-    # "RelatieFilter",
 ]
