2c6d2c9e6d76e46fcdca2817d68aed72178df174
Dennis Bakhuis <git@bakhuis.nu>
2023-04-06 15:33:33 +0200

    added submodules to __init__.py

--- eboekhouden_python/__init__.py
+++ eboekhouden_python/__init__.py
@@ -1,8 +1,11 @@
 """Main init for eboekhouden_python."""
 from .eboekhouden_client import EboekhoudenClient
+from . import models
+from . import constants
 
-
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 __all__ = [
     "EboekhoudenClient",
+    "models",
+    "constants",
 ]
