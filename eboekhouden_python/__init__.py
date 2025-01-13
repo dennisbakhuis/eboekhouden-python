809e910a484c92a2e5ead6e1866577679414a81c
Dennis Bakhuis <git@bakhuis.nu>
2025-01-13 14:49:07 +0100

    version bump

--- eboekhouden_python/__init__.py
+++ eboekhouden_python/__init__.py
@@ -3,7 +3,7 @@ from .eboekhouden_client import EboekhoudenClient
 from . import models
 from . import constants
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __all__ = [
     "EboekhoudenClient",
     "models",
