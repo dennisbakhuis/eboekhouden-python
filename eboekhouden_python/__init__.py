adef40ee4f35ad4f93b596200ea6746a50f02dab
Dennis Bakhuis <git@bakhuis.nu>
2025-09-05 15:38:42 +0200

    added missing test; adding workflow to publish to pypi

--- eboekhouden_python/__init__.py
+++ eboekhouden_python/__init__.py
@@ -3,7 +3,7 @@ from .eboekhouden_client import EboekhoudenClient
 from . import models
 from . import constants
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 __all__ = [
     "EboekhoudenClient",
     "models",
