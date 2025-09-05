c91c4f9e7e78bac18b4e844b539861d32818f7c2
Dennis Bakhuis <git@bakhuis.nu>
2025-09-05 14:32:02 +0200

    added missing test; adding workflow to publish to pypi

--- tests/test_version_same_as_pyproject.py
+++ tests/test_version_same_as_pyproject.py
@@ -6,4 +6,4 @@ from eboekhouden_python import __version__
 def test_version_same_as_pyproject():
     with open("pyproject.toml", "rb") as f:
         pyproject = tomli.load(f)
-    assert __version__ == pyproject["tool"]["poetry"]["version"]
+    assert __version__ == pyproject["project"]["version"]
